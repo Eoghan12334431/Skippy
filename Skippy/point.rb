c3c973215ef1214414c02fece24a13bfa48dcb74
Eoghan12334431 <eoghan.mc-carey@ucdconnect.ie>
2014-10-01 12:23:20 +0100

    skippy

--- Skippy/point.rb
+++ Skippy/point.rb
@@ -2,27 +2,26 @@ require_relative 'grid'
 
 class Point
 
-  attr_accessor :direction
+  attr_accessor :p1, :p2
 
-  def initialize(p1, p2)
-    p1 = 0
-    p2 = 0
+  def initialize
+    @p1 = 0
+    @p2 = 0
   end
 
   roll = Die.direction
-  def move!
-    if roll == 'north'
-      Point.new(p1+1,p2)
-
+  def move!(current_direction)
+    if current_direction == 'north'
+      @p1 += 1
     end
-    if roll == 'south'
-      Point.new(p1-1,p2)
+    if current_direction == 'south'
+      @p1 -= 1
     end
-    if roll == 'east'
-      Point.new(p1,p2+1)
+    if current_direction == 'east'
+      @p2 += 1
     end
-    if roll == 'west'
-      Point.new(p1,p2-1)
+    if current_direction == 'west'
+      @p2 -= 1
     end
   end
 end
